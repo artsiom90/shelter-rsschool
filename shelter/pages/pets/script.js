13cfc6617ba08d9f1713ca5e43b6d7ba0edda6a4
artsiom90 <artsiom.trush@gmail.com>
2022-04-02 14:08:43 +0300

    feat: add mobile styles and burger menu on pets page

---
+++ shelter/pages/pets/script.js
@@ -0,0 +1,37 @@
+//menu
+const navbarMenu = document.querySelector('.header-navbar-pets')
+const navbarLinks = document.querySelectorAll('.navbar-list-pets')
+const openMenuBtn = document.querySelector('.header-burger-pets')
+const closeMenuBtn = document.querySelector('.header-burger-pets-close')
+
+openMenuBtn.addEventListener('click', () => {
+    navbarMenu.classList.add('navbar-open')
+    document.querySelector('.header-logo-pets').style.display = 'none'
+    document.querySelectorAll('.navbar-item-link').forEach(item => {
+        item.classList.add('navbar-mobile-color')
+    })
+    document.querySelectorAll('.burger-line').forEach(line => {
+        line.style.backgroundColor = 'white'
+    })
+    document.body.style.overflow = 'hidden'
+})
+
+closeMenuBtn.addEventListener('click', () => {
+    navbarMenu.classList.remove('navbar-open')
+    document.querySelector('.header-logo-pets').style.display = 'flex'
+    document.querySelectorAll('.navbar-item-link').forEach(item => {
+        item.classList.remove('navbar-mobile-color')
+    })
+    document.querySelectorAll('.burger-line').forEach(line => {
+        line.style.backgroundColor = 'black'
+    })
+    document.body.style.overflow = 'visible'
+})
+
+navbarLinks.forEach(link => link.addEventListener('click', e => {
+    if (e.target.classList.contains('navbar-item-link')
+        || e.target.classList.contains('logo-title')) {
+        navbarMenu.classList.remove('navbar-open')
+        document.body.style.overflow = 'visible'
+    }
+}))
