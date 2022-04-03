0fcec911f67fe6df4533d893e950fd934cbc6cc3
artsiom90 <artsiom.trush@gmail.com>
2022-04-03 21:26:24 +0300

    feat: implement slider logic

--- shelter/pages/main/script.js
+++ shelter/pages/main/script.js
@@ -1,3 +1,5 @@
+import petsData from '../../../petsData.json' assert { type: "json" }
+
 //menu
 const navbarMenu = document.querySelector('.header-navbar')
 const navbarLinks = document.querySelectorAll('.navbar-list')
@@ -23,3 +25,50 @@ navbarLinks.forEach(link => link.addEventListener('click', e => {
         document.body.style.overflow = 'visible'
     }
 }))
+
+//slider
+const sliderCardsSection = document.querySelector('.section-pets-slider-cards')
+const sliderCards = document.querySelectorAll('.section-pets-slider-card')
+const btnNext = document.querySelectorAll('.button-next')
+const btnPrev = document.querySelectorAll('.button-prev')
+
+const cards = petsData.map(card => {
+    const template = `
+    <div class="section-pets-slider-card">
+        <img src=${card.img} alt="img">
+            <div class="slider-card-name">${card.name}</div>
+        <button class="button-transparent">Learn more</button>
+    </div>
+    `
+    return template
+})
+
+const prevCardsIndices = [0, 1, 2]
+
+const getUniqueRandomIndices = arraySize => {
+    let randomIndices = Array.apply(null, Array(arraySize))
+        .map(() => Math.floor((Math.random() * (petsData.length)) + 0))
+    const isMatching = prevCardsIndices && prevCardsIndices.some(prevIndex => randomIndices.includes(prevIndex))
+    if (new Set(randomIndices).size !== randomIndices.length || isMatching) {
+        randomIndices = getUniqueRandomIndices(arraySize)
+    }
+    return randomIndices
+}
+
+const changeSlides = () => {
+    const indices = []
+    if (sliderCards.length === 3) indices.push(...getUniqueRandomIndices(3))
+    if (sliderCards.length === 2) indices.push(...getUniqueRandomIndices(2))
+    if (sliderCards.length === 1) indices.push(...getUniqueRandomIndices(1))
+
+    sliderCardsSection.innerHTML = ''
+    indices.forEach(index => {
+        sliderCardsSection.innerHTML += cards[index]
+    })
+    prevCardsIndices.length = 0
+    prevCardsIndices.push(...indices)
+}
+
+btnNext.forEach(btn => btn.onclick = () => changeSlides())
+
+btnPrev.forEach(btn => btn.onclick = () => changeSlides())
